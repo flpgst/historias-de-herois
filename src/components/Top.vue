e3ea1345a678a4a05b90c8b19ab7fa3541a703d0
Filipe Augusto Gonçalves <filipe-augusto-goncalves@impulso.network>
2022-02-16 23:12:32 -0300

    refactor: Altera estilo do título da home

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -3,11 +3,11 @@
     <v-col cols="3" class="d-flex justify-center">
       <logo></logo>
     </v-col>
-    <v-col cols="6" class="d-flex align-center text-justify">
-      <span class="text-h5">
-        Aqui contamos as histórias dos super-heróis com uma linguagem limpa e acessível para
-        crianças em idade pré-escolar.
-      </span>
+    <v-col cols="6" class="d-flex flex-column align-center justify-center">
+      <div class="text-h4">Um lugar com histórias de super-heróis e vilões.</div>
+      <div class="text-h6">
+        Adaptadas para uma linguagem limpa e acessível para crianças em idade pré-escolar.
+      </div>
     </v-col>
   </v-row>
 </template>
