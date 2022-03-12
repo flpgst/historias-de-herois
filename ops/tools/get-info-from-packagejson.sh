95f7a38f5b59ae45e2e43e9c9cdaa2615fc10df6
Filipe Augusto Gonçalves <filipe-augusto-goncalves@impulso.network>
2022-03-12 00:53:09 -0300

    fix: remove config de deploy não utilizada

--- ops/tools/get-info-from-packagejson.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-export SERVICE=${SERVICE_NAME:-$(cat ${GITHUB_WORKSPACE}/package.json  | grep name | head -1 | sed 's/"name"://' | sed 's/[",]//g'| tr -d '[[:space:]]')}
-export PACKAGE_VERSION=$(cat ${GITHUB_WORKSPACE}/package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
-echo "Service is $SERVICE:$PACKAGE_VERSION"
